44c88e010226905ff64296d43f39500e394502e2
Neil Gupta <neil@hiome.com>
2019-09-03 16:06:09 -0500

    add ackee analytics

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,14 @@
  */
 
 // You can delete this file if you're not using it
+
+import React from "react"
+
+export const onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
+  setPostBodyComponents([
+    <script key="ackee" async
+      src="https://a.hiome.com/tracker.js"
+      data-ackee-server="https://a.hiome.com"
+      data-ackee-domain-id="d9b047a7-d3cc-4d28-bdb1-eb16729b382e" />
+  ])
+}
